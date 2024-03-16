b9ae2391e5aed30d90f6b3878f2e1f2a6fdefa18
Eoghan West <eowest@gmail.com>
2024-03-16 03:35:11 -0400

    added to enum message type enum

--- sherlock/src/message_bus/messages.rs
+++ sherlock/src/message_bus/messages.rs
@@ -18,12 +18,19 @@ fn empty_map() -> HashMap<String, Value> {
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum SherlockMessageType {
+    /// Request to speak utterance
     #[serde(rename = "speak")]
     Speak,
+    /// Speak message data as audio, emitted by the TTS Node.
     #[serde(rename = "spoken")]
     Spoken,
+    /// Internet connection is now available (only generated on initial connection)
     #[serde(rename = "mycroft.internet.connected")]
     MycroftInternetConnected,
+    /// Sent by start-up sequence when everything is ready for user interaction
     #[serde(rename = "mycroft.ready")]
     MycroftReady,
+    /// Stop command (e.g. button pressed)
+    #[serde(rename = "mycroft.stop")]
+    MycroftStop,
 }
